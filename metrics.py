b718de7827aa03b980e3dfe7a04df9e589e14768
Mars <marshall.harvey@outlook.com>
2021-01-29 13:23:55 +0000

    updated library and added script to call api

---
+++ metrics.py
@@ -0,0 +1,9 @@
+import metrics_api
+import time
+
+while True:
+    metrics_api.get_sent_bytes_prod()
+    metrics_api.get_received_bytes_prod()
+    metrics_api.get_sent_bytes_dev()
+    metrics_api.get_received_bytes_dev()
+    time.sleep(300)
---
+++ metrics_api.py
@@ -0,0 +1,146 @@
+import requests
+from datetime import datetime, timedelta
+
+now = datetime.now()
+TIME = format(now - timedelta(hours=3), "%H:%M:%S")
+DATE = now.strftime("%Y-%m-%d")
+INTERVAL_TIME = format(now - timedelta(minutes=30), "%H:%M:%S")
+API_KEY = ''
+secret_key = ''
+URL = "https://api.telemetry.confluent.cloud/v1/metrics/cloud/"
+
+def get_available_metrics():
+    response = requests.get(URL + "descriptors", auth=(API_KEY, secret_key))
+    print(response.status_code)
+    return response
+
+def get_received_bytes_dev():
+    query = {
+         "aggregations": [
+            {
+                "agg": "SUM",
+                "metric": "io.confluent.kafka.server/received_bytes"
+            }
+        ],
+        "filter": {
+            "filters": [
+                {
+                    "field": "metric.label.cluster_id",
+                    "op": "EQ",
+                    "value": "lkc-"
+                 }
+            ],
+            "op": "AND"
+        },
+        "granularity": "PT1M",
+        "group_by": [
+            "metric.label.topic"
+        ],
+        "intervals": [
+            f"{DATE}T{TIME}-00:00/{DATE}T{INTERVAL_TIME}-00:00"
+        ],
+        "limit": 25
+    }
+    response = requests.post(URL + "query", auth=(API_KEY, secret_key), json=query)
+    print(response.status_code)
+    return response
+
+def get_sent_bytes_dev():
+    query = {
+         "aggregations": [
+            {
+                "agg": "SUM",
+                "metric": "io.confluent.kafka.server/sent_bytes"
+            }
+        ],
+        "filter": {
+            "filters": [
+                {
+                    "field": "metric.label.cluster_id",
+                    "op": "EQ",
+                    "value": "lkc-"
+                 }
+            ],
+            "op": "AND"
+        },
+        "granularity": "PT1M",
+        "group_by": [
+            "metric.label.topic"
+        ],
+        "intervals": [
+            f"{DATE}T{TIME}-00:00/{DATE}T{INTERVAL_TIME}-00:00"
+        ],
+        "limit": 25
+    }
+    response = requests.post(URL + "query", auth=(API_KEY, secret_key), json=query)
+    print(response.status_code)
+    return response
+
+def get_received_bytes_prod():
+    query = {
+         "aggregations": [
+            {
+                "agg": "SUM",
+                "metric": "io.confluent.kafka.server/received_bytes"
+            }
+        ],
+        "filter": {
+            "filters": [
+                {
+                    "field": "metric.label.cluster_id",
+                    "op": "EQ",
+                    "value": "lkc-"
+                }
+            ],  
+            "op": "AND"
+        },
+        "granularity": "PT1M",
+        "group_by": [
+            "metric.label.topic"
+        ],
+        "intervals": [
+            f"{DATE}T{TIME}-00:00/{DATE}T{INTERVAL_TIME}-00:00"
+        ],
+        "limit": 25
+    }
+    response = requests.post(URL + "query", auth=(API_KEY, secret_key), json=query)
+    print(response.status_code)
+    return response
+
+def get_sent_bytes_prod():
+    query = {
+         "aggregations": [
+            {
+                "agg": "SUM",
+                "metric": "io.confluent.kafka.server/sent_bytes"
+            }
+        ],
+        "filter": {
+            "filters": [
+                {
+                    "field": "metric.label.cluster_id",
+                    "op": "EQ",
+                    "value": "lkc-"
+                 }
+            ],
+            "op": "AND"
+        },
+        "granularity": "PT1M",
+        "group_by": [
+            "metric.label.topic"
+        ],
+        "intervals": [
+            f"{DATE}T{TIME}-00:00/{DATE}T{INTERVAL_TIME}-00:00"
+        ],
+        "limit": 25
+    }
+    response = requests.post(URL + "query", auth=(API_KEY, secret_key), json=query)
+    print(response.status_code)
+    return response
+    
+'''
+get_received_bytes_dev()
+get_sent_bytes_dev()
+get_received_bytes_prod()
+get_sent_bytes_prod()
+'''
